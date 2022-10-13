e5e8c154bb9d3cf7b4cc426f784ae5ad6d27ffcb
Adam Ascencio <52258246+adamascencio@users.noreply.github.com>
2022-10-13 16:51:05 -0700

    Time: 118 ms (22.89%), Space: 43.9 MB (22.66%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let answer = '';
+    let minWord = strs[0];
+    let minWordIdx = 0;
+    let minWordCount = strs[minWordIdx].length
+    let arr = [];
+    strs.forEach((word, idx) => {
+        word.length < minWord.length ? minWordIdx = idx : null;
+    });
+    while (minWordCount >= 0) {
+        strs.forEach(word => arr.push(word.slice(0, minWordCount + 1)));
+        let isPrefix = arr.every(word => word === arr[0]);
+        if (isPrefix) return arr[0];
+        minWordCount--;
+        arr = [];
+    }
+    return answer;
+};
