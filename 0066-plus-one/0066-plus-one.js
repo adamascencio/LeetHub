48c98149e7755228923b9591b2530788db9ee0cc
Adam Ascencio <52258246+adamascencio@users.noreply.github.com>
2022-10-26 10:20:53 -0700

    Time: 106 ms (27.14%), Space: 42.4 MB (6.53%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -3,7 +3,7 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    const addOne = BigInt(digits.join('')) + 1n;
-    const arr = Array.from(String(addOne), Number);
+    let addOne = BigInt(digits.join('')) + 1n;
+    let arr = Array.from(String(addOne), Number);
     return arr;
 };
