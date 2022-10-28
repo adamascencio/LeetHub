8b0d45c9e889684dbb17fc5773cd084e51f7de6c
Adam Ascencio <52258246+adamascencio@users.noreply.github.com>
2022-10-28 09:33:48 -0700

    Time: 139 ms (62.49%), Space: 50.4 MB (78.96%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -4,6 +4,5 @@
  */
 var containsDuplicate = function(nums) {
     const removeDups = [...new Set(nums)];
-    console.log(removeDups);
     return removeDups.length === nums.length ? false : true;
 };
