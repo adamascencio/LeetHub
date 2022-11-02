af429594817f2dfaf707bb78f5445b4ccb0c9a0e
Adam Ascencio <52258246+adamascencio@users.noreply.github.com>
2022-11-01 22:18:43 -0700

    Time: 116 ms (31.10%), Space: 43 MB (43.56%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    let output = Math.max(...nums);
+    let currMin = 1;
+    let currMax = 1;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0) {
+            currMin = 1;
+            currMax = 1;
+            continue;
+        }
+        let tmp = currMax * nums[i];
+        currMax = Math.max(nums[i] * currMax, nums[i] * currMin, nums[i]);
+        currMin = Math.min(tmp, nums[i] * currMin, nums[i]);
+        output = Math.max(output, currMax);
+    }
+    return output;
+};
